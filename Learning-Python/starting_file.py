564ca699dda1b1f6ef4a9b43d5f2ef54c32e98a6
John Doe <john.ozuna01@gmail.com>
2023-10-30 14:14:56 -0700

    tip calc

--- Learning-Python/starting_file.py
+++ Learning-Python/starting_file.py
@@ -3,8 +3,13 @@
 
 print("Welcome to the tip calculator.")
 
-total_bill = print(input("What was the total bill?"))
-percent = print(input("What percentage tip would you like to give? 10, 12, or 15?"))
-people = print(input("How many people to split the bill?"))
+bill = float(input("What was the total bill? "))
+percent = int(input("What percentage tip would you like to give? 10, 12, or 15 ?"))
+people = int(input("How many people to split the bill? "))
 
+bill_w_tip = ((bill * (percent/100)) + bill)
 
+bill_pp = (bill_w_tip / people)
+final_amt = "{:.2f}".format(bill_pp)
+
+print(f"Each personal should pay ${final_amt}.")
